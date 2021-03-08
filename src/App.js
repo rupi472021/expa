36df6a33648618d7e004c3e2e1e7f70e6773c40d
benmsh <77381491+benmsh@users.noreply.github.com>
2021-03-08 21:30:59 +0200

    ben 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FCSignIn from './FunctionComponents/FCSignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ben ya shmuk <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <FCSignIn />
     </div>
   );
 }

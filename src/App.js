47f70e7fafcf837915da72f4af7d9c5ccf897b32
igroup4 <rupi472021@gmail.com>
2021-03-08 20:59:15 +0200

    daniel update#2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ben <code>src/App.js</code> and save to reload.
+          ben ya shmuk <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

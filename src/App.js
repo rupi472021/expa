3f8d27a31eb8386de5bf38113746204ffbc9a12e
benmsh <77381491+benmsh@users.noreply.github.com>
2021-03-12 13:48:35 +0200

    ben

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Switch, Route ,withRouter } from 'react-router-dom';
 import FCSignIn from './FunctionComponents/FCSignIn';
 import FCRegister from './FunctionComponents/FCRegister';
+import ResetPasswordPage from './ClassComponents/ResetPasswordPage';
 
 function App() {
   return (
@@ -11,8 +12,11 @@ function App() {
           <Route  exact path="/" >
             <FCSignIn />
           </Route>
-          <Route  exact path="/register" >
+          <Route exact path="/register" >
             <FCRegister />
+          </Route>
+          <Route exact path="/forget_password_page" >
+            <ResetPasswordPage />
           </Route>
         </Switch>
       </header>

c9e4f694d30f3b6d34bf3f985ced8ad7ed37c307
igroup4 <rupi472021@gmail.com>
2021-03-17 13:06:33 +0200

    CCRegister 17.03

--- src/ClassComponents/CCRegisterPage.jsx
+++ src/ClassComponents/CCRegisterPage.jsx
@@ -6,11 +6,15 @@ import 'fontsource-roboto';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { makeStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import Link from '@material-ui/core/Link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import Checkbox from '@material-ui/core/Checkbox';
 // import Link from '@material-ui/core/Link';
@@ -27,8 +31,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import PictureUploader from '../Components/PictureUploader';
 import { Row } from 'reactstrap';
 import { Col } from 'react-bootstrap';
+import classes from './BlogCard.module.css';
 
 import '../MyStyle.css';
+import { CssBaseline } from '@material-ui/core';
 
 
 export default class CCRegisterPage extends Component {
@@ -41,79 +47,117 @@ export default class CCRegisterPage extends Component {
     }
 
     componentDidMount() {
-        // alert("hey");
+        alert("hey");
+        window.scrollTo(0, 0);
+
     }
 
-     scrolltotop = () => {
+    scrolltotop = () => {
         // window.querySelector('body').scrollTo(0,0)
         document.getElementById("scroller").scroll(0, 0)
 
     }
 
+    goTO = () => {
+        window.scrollTo({ top: 500, behavior: 'smooth' });
+
+    }
+
 
     render() {
-        const { classes } = this.props;
+        // const { classes } = this.props;
         // const commentSection = useRef(null);
         // const gotoCommentSection = () => window.scrollTo({ top: commentSection.current.offsetTop, behavior: "smooth" });
         return (
-            
-            <Container component="main" /*maxWidth="s"*/ style={{ backgroundColor: "#ffdf80" }}>
-                <Button variant="primary" size="sm" href="/" className="but" style={{ background: "green", }} /*onClick={() => HandleClick()}*/>Back</Button>
-                
-                <div className="paper">
+            // <div className={classes.NewBLogCard}>
+            //     <Container>
+            //         {/* <Container component="main" maxWidth="s" style={{ backgroundColor: "#ffdf80" }}> */}
+            //         <CssBaseline />
+            //         <div className={classes.Container}>
+            //             <Button variant="primary" size="sm" href="/" className="but" style={{ background: "green", }} /*onClick={() => HandleClick()}*/>Back</Button>
 
-                    <h1 className="ExPa" > Create an Account </h1>
-                    <Form className="form" noValidate>
-                        {/* <Avatar alt="Remy Sharp" src="https://i.ibb.co/7S6XfNZ/circle-cropped.png" className={classes.large} /> */}
-                        {/* <div className="container">
-                        <h3>bezkoder.com</h3>
-                        <h4>React Image Upload with Preview</h4>
+            //             {/* <div className="paper"> */}
+            //             <form>
+            //                 <h1 className="ExPa" > Create an Account </h1>
+            //                 {/* <Form className="form" noValidate> */}
+            //                 {/* <Avatar alt="Remy Sharp" src="https://i.ibb.co/7S6XfNZ/circle-cropped.png" className={classes.large} /> */}
+            //                 {/* <div className="container">
+            //             <h3>bezkoder.com</h3>
+            //             <h4>React Image Upload with Preview</h4>
 
-                        <div className="content">
-                            
-                            <UploadImages/>
-                        </div>
-                    </div> */}
-                        <Row>
-                            <Col xs={6} md={4}>
-                                <Image src="../logo.svg/171x180" roundedCircle />
-                            </Col>
+            //             <div className="content">
 
-                        </Row>
-                        {/* <PictureUploader className={classes.ProfilePhoto} /> */}
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="fName" label="First Name" name="fName" autoComplete="First Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="email" label="Email address" name="email" autoComplete="Email" autoFocus />
+            //                 <UploadImages/>
+            //             </div>
+            //         </div> */}
+            //                 <Row>
+            //                     <Col xs={6} md={4}>
+            //                         <Image src="../logo.svg/171x180" roundedCircle />
+            //                     </Col>
 
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
-                        {/* <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" /> */}
-                        <Button type="submit" fullWidth variant="contained" className="submit"> lets keep going </Button>
+            //                 </Row>
+            //                 <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
 
-                        {/* <input name="user[email]" autocapitalize="off" required="" class="form-control input py-1" value="" type="text" id="user_email" autocomplete="off" spellcheck="false" aria-describedby=" "></input> */}
-                        <Button id="scroller" variant="primary" size="md" className="but" onClick={this.scrolltotop}>Let's know you better </Button>
-                        <Button fullWidth variant="primary"  size="large"> Sign In </Button>
+            //                 {/* <PictureUploader className={classes.ProfilePhoto} /> */}
+            //                 <TextField size="small" className="TextField" variant="outlined" margin="normal" required fullWidth id="fName" label="First Name" name="fName" autoComplete="First Name" autoFocus />
+            //                 <TextField size="small" className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField size="small" className="TextField" variant="outlined" margin="normal" required fullWidth id="email" label="Email address" name="email" autoComplete="Email" autoFocus />
 
-                    </Form>
+            //                 <TextField size="small" className="TextField" variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+            //                 {/* <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" /> */}
+            //                 {/* <Button type="submit" fullWidth variant="contained" className="submit"> lets keep going </Button> */}
 
-                    <Form className="que" noValidate>
-                        <TextField ref={this.commentSection} className="TextField" variant="outlined" margin="normal" required fullWidth id="fName" label="Would you travel with another " name="fName" autoComplete="First Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
-                        <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 {/* <input name="user[email]" autocapitalize="off" required="" class="form-control input py-1" value="" type="text" id="user_email" autocomplete="off" spellcheck="false" aria-describedby=" "></input> */}
+            //                 {/* <Button id="scroller" variant="primary" size="md" className="but" onClick={this.scrolltotop}>Let's know you better </Button> */}
+            //                 <Button fullWidth variant="primary" size="large" onClick={this.goTO}> Let's know you better </Button>
+            //             </form>
+            //             {/* </Form> */}
 
-                    </Form>
-                </div>
-            </Container >
+            //             {/* <Form className="que" noValidate>
+            //                 <TextField ref={this.commentSection} className="TextField" variant="outlined" margin="normal" required fullWidth id="fName" label="Would you travel with another " name="fName" autoComplete="First Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+            //                 <TextField className="TextField" variant="outlined" margin="normal" required fullWidth id="lName" label="Last Name" name="lName" autoComplete="Last Name" autoFocus />
+
+            //             </Form> */}
+            //             {/* </div> */}
+            //         </div>
+            //     </Container >
+            // </div>
+
+
+            <div className={classes.NewBLogCard}>
+                <Container>
+                    <CssBaseline />
+                    <div className={classes.Container} >
+                    <Button variant="secondary" size="sm" href="/" className="but" /*onClick={() => HandleClick()}*/>Back</Button>
+                        <Avatar alt="Remy Sharp" src="https://i.ibb.co/7S6XfNZ/circle-cropped.png" style={{ width: '15vh', height: '15vh', marginTop: '10px' }} />
+                                        <h1 className="ExPa" > Create an Account </h1>
+
+                        <form>
+                            <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+                            <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+                            <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+                            <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+                            <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+                            <TextField style={{ backgroundColor: 'white' }} variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+
+
+                            <Button className={classes.ButtonSignIn} fullWidth variant="contained" color="Primary" size="large" onClick={this.signinbtn}> Sign In </Button><br></br><br></br><br></br>
+
+                        </form>
+                    </div>
+                </Container >
+            </div>
         )
     }
 }
 
-// const useStyles=makeStyles(theme=>({
 
 
 

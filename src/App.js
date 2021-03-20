4ceb1765a982a5cd6f5061112ffe75f2d38b2355
igroup4 <rupi472021@gmail.com>
2021-03-20 11:37:04 +0200

    daniel 20.03

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import FCSignIn from './FunctionComponents/FCSignIn';
 import CCResetPasswordPage from './ClassComponents/CCResetPasswordPage';
-import FCRegister from './FunctionComponents/FCRegister';
 import PictureUploader from './Components/PictureUploader';
 import CCRegisterPage from './ClassComponents/CCRegisterPage';
 import CCLoginPage from './ClassComponents/CCLoginPage';

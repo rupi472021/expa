e78448dc52f46a7aa8ab113ca69005c6f4993478
igroup4 <rupi472021@gmail.com>
2021-04-18 20:09:24 +0300

    Update FCMainMenuPage.jsx

--- src/FunctionComponents/FCMainMenuPage.jsx
+++ src/FunctionComponents/FCMainMenuPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Swal from 'sweetalert2';
 import { Button } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import shadows from '@material-ui/core/styles/shadows';
 
 export default function FCMainMenuPage() {
 
@@ -29,14 +30,14 @@ export default function FCMainMenuPage() {
         <div style={{ backgroundColor: '#1d21243b', height: '100vh', alignItems: 'center' }}>
             <div style={{ padding: 5 }}><Button size="sm" onClick={backbtn}>BACK</Button></div>
             <div style={{ textAlign: 'center' }}><br></br>
-                <img src={localStorage.getItem('user_image')} alt={true} style={{ width: '45%', borderRadius: 100, borderWidth: 2, borderStyle: 'solid' }} /><br></br><br></br>
+                <img src={localStorage.getItem('user_image')} alt={true} style={{ width: '45%', borderRadius: 100, borderWidth: 2, borderStyle: 'solid' }} /><br></br><br></br><br></br>
                 <p>
-                    <h5 style={{ marginLeft: 7 }}>Hi {localStorage.getItem('user_fname')} {localStorage.getItem('user_lname')}, Welcome To ExPa!</h5>
+                    {/* <h5 style={{ marginLeft: 7 }}>Hi {localStorage.getItem('user_fname')} {localStorage.getItem('user_lname')}, Welcome To ExPa!</h5> */}
+                    <Button style={{ boxShadow:'0 0 50px 10px #141414',marginLeft: 7,width: '90%', borderRadius: 10, borderWidth: 5, fontWeight: 'bold', fontSize: '20px' }} fullWidth color="info" size="sm" disabled='false' >Hi {localStorage.getItem('user_fname')} {localStorage.getItem('user_lname')}, Welcome To ExPa!</Button>
                 </p>
             </div>
             <div>
                 <div style={{ marginTop: 100 }}>
-                
                     {/* <Button fullWidth disabled={true} style={{ alignItems: "center" }} size="lg"  >Join Trip</Button><br></br><br></br><br></br> */}
                     <Button style={{ width: '60%', borderRadius: 20, borderWidth: 5, fontWeight: 'bold' }} disabled={true} size="lg" >Join Trip</Button><br></br><br></br>
 
@@ -44,7 +45,7 @@ export default function FCMainMenuPage() {
                     <Button style={{ width: '60%', borderRadius: 20, borderWidth: 5, fontWeight: 'bold' }} fullWidth color="info" size="lg" href='/main_menu_page/create_new_trip_page' >New Trip</Button>
 
                 </div>
-                <div style={{ marginTop: 150 }}>
+                <div style={{ marginTop: 130 }}>
                 <Button style={{ width: '40%', borderRadius: 20, borderWidth: 5, fontWeight: 'bold' }} fullWidth color="info" size="lg" href="/main_menu_page/my_profile" >My Profile</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <Button style={{ width: '40%', borderRadius: 20, borderWidth: 5, fontWeight: 'bold' }} fullWidth color="info" size="lg" href="/main_menu_page/my_trips" >My Trips</Button>
 
